fc9b772f11afa3673bce360219eb84ae380f3405
yashtamakuwala <neel64tamakuwala@gmail.com>
2019-07-23 22:52:17 +1000

    Assignment spec and helper classes

--- Lsr.py
+++ Lsr.py
@@ -1,15 +1,36 @@
 #!usr/bin/python3
+from socket import *
+
+
+UPDATE_INTERVAL = 1  #1 secs
+ROUTE_UPDATE_INTERVAL = 30  #in secs
 
 class Neighbour:
     neighbourName = ""
     port = 0    #has to be integer
-    costToReach = -1.0  #has to be float with 1 decimal place
+    costToReach = -1.0  #has to be float with 1 decimal place, will not change after initialisation
+    # cost is bidrection => A->B = B->A
 
 class Router:
     routerName = ""
     port = 0
-    neighbourList = list(Neighbour) #will be list or dictionary?
+    neighbours = set(Neighbour) #will be list or dictionary?
 
+class Message:
+    messageText = ""
+    sender = ""
+    receivers = list(Router)
 
 def readFile(filename):
     return None
+
+#to be called every Route_update_interval and 2*Route_update_interval when topology changes
+def calculateDijkstraForNode(Router):
+    return None
+
+def printOutput(router):
+    print ("I am Router A")
+    # for a in set of routers:
+    print("Least cost path to router C:AFDC and the cost is 4.5")
+    return None
+
